c60c1a941923ddf550ac30b7eb1831a19a3d3c60
arorash <arorashashank9999@gmail.com>
2021-06-16 22:35:48 +0530

    change in dbcon

--- dbcon.php
+++ dbcon.php
@@ -1,9 +1,11 @@
 <?php
-    $server = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db = 'xkcd_db';
+    $server = 'us-cdbr-east-04.cleardb.com';
+    $user = 'b1d2893113532f';
+    $pass = '70678d3b';
+    $db = 'heroku_6f831fa5ac72ac8';
 
     $con = mysqli_connect($server,$user,$pass,$db);
-    
+
+
+
 ?>
