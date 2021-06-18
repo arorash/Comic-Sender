0c1ede3b28f7d5c8ca7fd5e053ec438b1869807e
arorash <arorashashank9999@gmail.com>
2021-06-18 22:26:00 +0530

    changes in sendcomics

--- sendcomics.php
+++ sendcomics.php
@@ -1,4 +1,11 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
     include 'dbcon.php';
 
     $url1 = "http://xkcd.com/info.0.json";
@@ -10,37 +17,41 @@
     $json2 = file_get_contents($url);
     $json2 = json_decode($json2);
 
-    $semi_rand = md5(time());
-    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-
-    $sub = "Comic - ".$json2->title;
-
-    $image = "Content-Type: application/octet-stream; name=\"".basename($json2->img)."\"\n" .
-        "Content-Description: ".basename($json2->img)."\n" .
-        "Content-Disposition: attachment;\n" . " filename=\"".basename($json2->img)."\"; \n" .
-        "Content-Transfer-Encoding: base64\n\n" . chunk_split(base64_encode(file_get_contents($json2->img))) . "\n\n";
-
-    $sender = "From: shashankprofessional9@gmail.com";
-    $sender .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
 
     $selectquery = "select * from registration where status='active'";
     $squery = mysqli_query($con,$selectquery);
 
     while($row = mysqli_fetch_assoc($squery)){
         $to_ = $row['email'];
-        $to = "$to_";
         $token = $row['token'];
-        $body = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-        "Content-Transfer-Encoding: 7bit\n\n" ."Title : ".$json2->title. " <br><br> <img src='".$json2->img."' /><br><br>If you want to stop getting these comics, please click the below link: <br><a href='http://localhost:7882/XKCD_Project/unsubscribe.php?token=$token''>Unsubscribe</a> ". "\n\n";
-        $body .= "--{$mime_boundary}\n";
-        $body .= $image;
+        $subject = "Comic - $json2->title";
+        $body = "Title : ".$json2->title. " <br><br> <img src='".$json2->img."' /><br><br>If you want to stop getting these comics, please click the below link: <br><a href='https://xckd-comic-project.herokuapp.com/unsubscribe.php?token=$token''>Unsubscribe</a> ";
 
-        if(mail($to,$sub,$body,$sender)){
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "shashankprofessional9@gmail.com";
+        $mail->Password = "Sha1Sha2nk@78";
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+        $mail->isHTML(true);
+        $mail->From = "shashankprofessional9@gmail.com";
+        $mail->FromName = "XCKD COMIC PROJECT";
+        $mail->addAddress("$to_");
+        $mail->Subject = $subject;
+        $mail->Body =$body;
+
+        $mail->addStringAttachment(file_get_contents($json2->img), basename($json2->img),'base64', 'application/octet-stream');
+
+        try{
+            $mail->send();
             echo "sent";
-        }else{
-            echo "failed";
+        }catch(Exception $e){
+            echo "Error occured.";
         }
     }
+
 
 
 ?>
